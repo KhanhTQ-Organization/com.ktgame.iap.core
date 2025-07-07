78a011f328ed350a42f45ce4626e1545ffb05695
KhanhTQ-hub <khanhquang0912qh@gmail.com>
2025-07-07 10:00:40 +0700

    fix: update mockup purchase test

---
+++ Runtime/Mockup/MockupProduct.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+
+namespace com.ktgame.iap.core
+{
+	public class MockupProduct : IProduct
+	{
+		public string Id { get; }
+		public PurchaseType Type { get; }
+		public decimal LocalizedPrice { private set; get; }
+		public string IsoCurrencyCode { private set; get; }
+		public string LocalizedPriceString { private set; get; }
+		public string LocalizedTitle { private set; get; }
+		public string LocalizedDescription { private set; get; }
+		public bool HasReceipt => false;
+		public string Receipt => string.Empty;
+		public bool IsOwnedNonConsumable => false;
+		public bool IsSubscribed => false;
+
+		public MockupProduct(ProductData product)
+		{
+			Id = product.Id;
+			Type = product.Type;
+			LocalizedPriceString = product.Price;
+			IsoCurrencyCode = "$";
+			if (LocalizedPriceString.Contains(IsoCurrencyCode))
+			{
+				LocalizedPrice = decimal.Parse(LocalizedPriceString.Replace(IsoCurrencyCode, ""), CultureInfo.InvariantCulture);
+			}
+		}
+
+		public void SetLocalizedTitle(string localizedTitle)
+		{
+			LocalizedTitle = localizedTitle;
+		}
+
+		public void SetLocalizedDescription(string localizedDescription)
+		{
+			LocalizedDescription = localizedDescription;
+		}
+	}
+}
---
+++ Runtime/Mockup/MockupPurchaseValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace com.ktgame.iap.core
+{
+	public class MockupPurchaseValidator : IPurchaseValidator
+	{
+		public PurchaseState Validate(string productId, string productType, string receipt)
+		{
+			return PurchaseState.Purchased;
+		}
+	}
+}
